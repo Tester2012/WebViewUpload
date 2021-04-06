e4e693d6b85454dadbfdcc81465b5d973847585d
resi234dent@yandex.com <resi234dent@yandex.com>
2021-04-06 16:24:09 +0200

    Simplify code and use AutoClosable stream

--- app/src/main/java/com/example/webviewupload/ParcelFileDescriptorUtils.java
+++ app/src/main/java/com/example/webviewupload/ParcelFileDescriptorUtils.java
@@ -5,10 +5,10 @@ import android.util.Log;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.concurrent.Semaphore;
 
 public class ParcelFileDescriptorUtils {
     public static ParcelFileDescriptor pipeFrom(InputStream inputStream)
@@ -16,9 +16,13 @@ public class ParcelFileDescriptorUtils {
         ParcelFileDescriptor[] pipe = ParcelFileDescriptor.createPipe();
         ParcelFileDescriptor readPart = pipe[0];
         ParcelFileDescriptor writePart = pipe[1];
+        final Semaphore semaphore = new Semaphore(1);
 
-        new TransferThread(-1, inputStream, new FileOutputStream(writePart.getFileDescriptor()))
+        new TransferThread(-1, inputStream, new ParcelFileDescriptor.AutoCloseOutputStream(writePart),
+                semaphore)
                 .start();
+
+        semaphore.acquireUninterruptibly();
 
         return readPart;
     }
@@ -28,47 +32,43 @@ public class ParcelFileDescriptorUtils {
         final InputStream mIn;
         final OutputStream mOut;
         long fileId;
+        private final Semaphore semaphore;
 
-        TransferThread(long fileId, InputStream in, OutputStream out) {
+        TransferThread(long fileId, InputStream in, OutputStream out, Semaphore semaphore) {
             super("Transfer Thread");
             mIn = new BufferedInputStream(in);
             mOut = new BufferedOutputStream(out);
             this.fileId = fileId;
+            this.semaphore = semaphore;
 
             setDaemon(true);
         }
 
         @Override
         public void run() {
-            byte[] buf = new byte[1024];
+            byte[] buf = new byte[4096];
             int len;
 
             try {
                 while (true) {
                     len = mIn.read(buf);
-
                     if (len < 0) {
                         break;
                     }
-
                     mOut.write(buf, 0, len);
-                    mOut.flush(); // just to be safe
+                    mOut.flush();
                 }
-
-                mOut.flush(); // just to be safe
+                mOut.flush();
             } catch (IOException e) {
                 Log.i(getName(), "writing failed " + e.getMessage());
             } finally {
                 try {
                     mIn.close();
-                } catch (IOException e) {
-                    Log.i(getName(), e.getMessage());
-                }
-                try {
                     mOut.close();
                 } catch (IOException e) {
                     Log.i(getName(), e.getMessage());
                 }
+                semaphore.release();
             }
         }
     }
