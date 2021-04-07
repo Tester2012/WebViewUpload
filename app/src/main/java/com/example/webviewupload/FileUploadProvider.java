cfc3bbec22f9b76990cc2dd2afbb7a37d8f114e0
resi234dent@yandex.com <resi234dent@yandex.com>
2021-04-07 10:58:47 +0200

    Add an addtional not working approach

--- app/src/main/java/com/example/webviewupload/FileUploadProvider.java
+++ app/src/main/java/com/example/webviewupload/FileUploadProvider.java
@@ -4,6 +4,7 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.MemoryFile;
 import android.os.ParcelFileDescriptor;
 import android.provider.OpenableColumns;
 import android.text.TextUtils;
@@ -14,10 +15,15 @@ import androidx.annotation.Nullable;
 import org.apache.commons.io.IOUtils;
 
 import java.io.File;
+import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
 import java.util.List;
 
 public class FileUploadProvider extends ContentProvider {
@@ -73,21 +79,50 @@ public class FileUploadProvider extends ContentProvider {
         File file = new File(getContext().getApplicationContext().getCacheDir(),
                 TextUtils.join(File.separator, segments));
 
-        InputStream inputStream = new FileInputStream(file);
-        ParcelFileDescriptor[] pipe;
-        try {
-            pipe = ParcelFileDescriptor.createPipe();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        ParcelFileDescriptor readPart = pipe[0];
-        ParcelFileDescriptor writePart = pipe[1];
-        try {
-            IOUtils.copy(inputStream, new ParcelFileDescriptor.AutoCloseOutputStream(writePart));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        // Approach 2
+//        ParcelFileDescriptor fileDescriptor1 = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
+//        return fileDescriptor1;
 
-        return readPart;
+        //Approach 3
+//        InputStream inputStream = new FileInputStream(file);
+//        ParcelFileDescriptor[] pipe;
+//        try {
+//            pipe = ParcelFileDescriptor.createReliablePipe();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        ParcelFileDescriptor readPart = pipe[0];
+//        ParcelFileDescriptor writePart = pipe[1];
+//        OutputStream outputStream = null;
+//        try {
+//            outputStream = new ParcelFileDescriptor.AutoCloseOutputStream(writePart);
+//            IOUtils.copy(inputStream, outputStream);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } finally {
+//            try {
+//                inputStream.close();
+//                outputStream.flush();
+//                outputStream.close();
+//            } catch (IOException e) {
+//                //
+//            }
+//        }
+//
+//        return readPart;
+
+        // Approach 4
+        try {
+            MemoryFile memoryFile = new MemoryFile(file.getName(), (int) file.length());
+            byte[] fileBytes = Files.readAllBytes(file.toPath());
+            memoryFile.writeBytes(fileBytes, 0, 0, (int) file.length());
+            Method method = memoryFile.getClass().getDeclaredMethod("getFileDescriptor");
+            FileDescriptor fileDescriptor = (FileDescriptor) method.invoke(memoryFile);
+            Constructor<ParcelFileDescriptor> constructor = ParcelFileDescriptor.class.getConstructor(FileDescriptor.class);
+            ParcelFileDescriptor parcelFileDescriptor = constructor.newInstance(fileDescriptor);
+            return parcelFileDescriptor;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
